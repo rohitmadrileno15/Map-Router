3aab56f3ab463c0eab0abeec5a8a8e11da62ba23
Rohit Saha <55949623+rohitmadrileno15@users.noreply.github.com>
2020-07-02 20:02:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
-# Download the helper library from https://www.twilio.com/docs/python/install
-from twilio.rest import Client
+import requests, json
+""" Mode of transport can be fastest, shortest, pedestrian, bicycle """
+
+""" By Deafult - mode of transport is fastest """
+
+def mainfunc(from_val=" " , to_val= " ", mode_transport = "fastest"):
+
+    url = f"https://www.mapquestapi.com/directions/v2/route?key=UbH7DkjS05mBLP5lrTcqEiAGtsvq7tA6&from={from_val}&to={to_val}&outFormat=json&ambiguities=ignore&routeType={mode_transport}&doReverseGeocode=false&enhancedNarrative=false&avoidTimedConditions=false"
+    r = requests.get(url)
+    result =(r.json())
+
+    try:
+
+        distance = result['route']['distance']
+        print(distance)
+        print("\n")
+        
+        blank_text=  " "
+        blank_text += "distance is  "+str(distance) +" KMs."
+        blank_text += "\n"
+
+        narratives = (result['route']['legs'][0]["maneuvers"])
+
+        for row in narratives:
+            blank_text+= (row['narrative'])
+            blank_text+="\n"
+
+        return blank_text
 
 
-# Your Account Sid and Auth Token from twilio.com/console
-# DANGER! This is insecure. See http://twil.io/secure
-account_sid = 'ACbf49fe14378f7451d0665173575f06fb'
-auth_token = 'e6cb096c1094ade8bf334a9823406cbb'
-client = Client(account_sid, auth_token)
-
-message = client.messages .create(body="Hi Rohit, This is a message from your PC, Keep Hustling.",from_='+15088124097',to='+918787898226')
-
-print(message)
+    except Exception as e:
+        err = ("Please check your locations or, the way you sent your messages, NOTICE that the messages have to sent in the fashion of ORIGIN to DESTINATION \n")
+        return err
